4ce757e7baee823043eeddcfa452850db36ca96d
Cashman1396 <Chrisashman96@gmail.com>
2021-02-01 21:29:48 -0500

    added logo

--- src/components/my-navbar/my-navbar.jsx
+++ src/components/my-navbar/my-navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav"
+import Image from 'react-bootstrap/Image'
 import "./my-navbar.styles.css"
+import Oricale from "../../assets/imgs/Oricale.webp"
+
 
 
 const MyNavbar = () => {
   return (
     <><Navbar fixed="top" collapseOnSelect expand="lg" variant="dark" className="animate-navbar nav-theme justify-content-between">
-    <Navbar.Brand href="#home">Chris Ashman</Navbar.Brand>
+    
+    <Navbar.Brand href="#home"><Image className="logo1" alt="logo" src={Oricale}  />Chris Ashman</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="ml-auto">
