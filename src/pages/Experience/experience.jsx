06e68549798bad4fbfa736b684201614530c88ae
Cashman1396 <Chrisashman96@gmail.com>
2021-02-18 11:33:50 -0500

    added title

--- src/pages/Experience/experience.jsx
+++ src/pages/Experience/experience.jsx
@@ -4,10 +4,12 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import L_Oricale from "../../assets/imgs/Oricale.webp"
 import Tilt from "react-tilt";
+import "./experience.styles.css"
 
 const Experience = () => {
     return (
-        <div>
+        <div id="experience">
+            <h1 className="pt-3 text-center font-details-b pb-3">EXPERIENCE</h1>
             
         </div>
     )
