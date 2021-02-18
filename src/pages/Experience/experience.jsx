3962c9461b3ecdfb334878e521a83d6d96542de0
Cashman1396 <Chrisashman96@gmail.com>
2021-02-18 11:31:36 -0500

    adding an experience page

---
+++ src/pages/Experience/experience.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
+import L_Oricale from "../../assets/imgs/Oricale.webp"
+import Tilt from "react-tilt";
+
+const Experience = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Experience
