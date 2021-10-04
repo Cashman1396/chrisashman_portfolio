89ca59732c448e2ce62393bd2b3d63fbd3725c87
Cashman1396 <Chrisashman96@gmail.com>
2021-10-04 13:17:22 -0400

    forgot something

--- src/pages/Experience/experience.jsx
+++ src/pages/Experience/experience.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import L_ORICALE from "../../assets/imgs/Oricale.webp"
+import L_DX from "../../assets/imgs/DXAgency.webp"
 import Tilt from "react-tilt";
 import "./experience.styles.css"
 
@@ -16,7 +17,7 @@ const Experience = () => {
                   <Tilt options={{ max: 25 }}>
                   <Card>
                     <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
-                        <Card.Img variant="top" className="img-resize" src={L_ORICALE} alt="Oricale logo" />
+                        <Card.Img variant="top" className="img-resize" src={L_DX} alt="Oricale logo" />
                     </Card.Header>
                     <Card.Body className="d-flex justify-content-center flex-column">
                         <div>
